b6669a6ef06406ff5663d14069a50a3637bc216a
Juanfer <jsebas1198@hotmail.com>
2024-01-28 20:44:04 -0300

    Add Swagger documentation for API operations

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,6 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
@@ -8,6 +10,15 @@ async function bootstrap() {
     origin: '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   });
+
+  const config = new DocumentBuilder()
+    .setTitle('API')
+    .setDescription('Outdefine Spellchecker API')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
   await app.listen(31337);
 }
 bootstrap();
--- src/spellcheck/controllers/spellcheck.controller.ts
+++ src/spellcheck/controllers/spellcheck.controller.ts
@@ -1,11 +1,25 @@
 import { Controller, Get, Param } from '@nestjs/common';
 import { SpellcheckService } from '../services/spellcheck.service';
-
+import { ApiOperation, ApiParam, ApiResponse, ApiTags } from '@nestjs/swagger';
+@ApiTags('spell')
 @Controller('spell')
 export class SpellcheckController {
   constructor(private readonly spellcheckService: SpellcheckService) {}
 
   @Get(':word')
+  @ApiParam({ name: 'word', description: 'The word to spellcheck' })
+  @ApiOperation({
+    summary: 'Spellcheck a word',
+    description: 'Returns spellcheck suggestions and correctness',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'If the word is found, suggestions are provided if necessary.',
+  })
+  @ApiResponse({
+    status: 404,
+    description: 'Word not found',
+  })
   spellcheckWord(@Param('word') word: string): {
     suggestions: string[];
     correct: boolean;
